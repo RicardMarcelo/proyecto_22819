c84a0939c1cdb76944dfb542b26d6f33469e4189
RicardMarcelo <solimovil04@gmail.com>
2022-10-14 17:18:55 -0300

    Agregamos ejemplos con parámetros

--- cac/urls.py
+++ cac/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('hola_mundo', views.hola_mundo),
-    path('mundial',views.mundial)
+    path('mundial',views.mundial),
+    path('saludar',views.saludar,name='saludar_por_defecto'),
+    path('saludar/<str:nombre>',views.saludar,name='saludar'),
+    path('proyectos/<int:año>/<int:mes>',views.ver_proyectos,name='ver_proyectos'),
 ]
--- cac/views.py
+++ cac/views.py
@@ -8,3 +8,15 @@ def hola_mundo(request):
 
 def mundial(request):
     return HttpResponse('VAMOS ARGENTINA')
+
+def saludar(request,nombre='Estimada Persona'):
+    return HttpResponse(f"""
+    <h1>Hola Mundo Django - {nombre}</h1>
+    <p>Estoy haciendo mi primera prueba</p>
+    """)
+
+def ver_proyectos(request,año,mes):
+    return HttpResponse(f"""
+    <h1>Proyectos del - {mes}/{año}</h1>
+    <p>Listado de proyectos</p>
+    """)
